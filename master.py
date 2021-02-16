39acb53a64e04a1765161ef1720a96599058f24a
Lucas Dias <lucasccdias0@gmail.com>
2021-02-16 14:46:17 -0300

    PEP8

--- master.py
+++ master.py
@@ -6,6 +6,7 @@ Created on Thu Feb 11 10:22:54 2021
 """
 
 import os
+import rpy2.robjects as robjects
 
 # Mude aqui para o diretório onde se encontra este script
 os.chdir('C:/Users/Lucas/Desktop/desigualdades/')
@@ -13,28 +14,26 @@ os.chdir('C:/Users/Lucas/Desktop/desigualdades/')
 # Mude aqui para o diretório onde encontra o R (como abaixo)
 os.environ['R_HOME'] = 'C:/Program Files/R/R-4.0.1/'
 
-import rpy2.robjects as robjects
 r = robjects.r
 
 os.mkdir('tmp/')
 os.mkdir('output/')
 
-#==============================================================================
+# =============================================================================
 
 
 # Índice de Equilíbrio Racial e de Gênero
 exec(open("./code/indices-equilibrio.py").read())
-exec(open("./code/mapa-ie-genero.py", encoding = "utf-8").read())
-exec(open("./code/mapa-ie-racial.py", encoding = "utf-8").read())
+exec(open("./code/mapa-ie-genero.py", encoding="utf-8").read())
+exec(open("./code/mapa-ie-racial.py", encoding="utf-8").read())
 
 
 # Série do Índice de Gini por estado [demora +/- 1h e não roda na SDE]
 r['source']('./code/gini.R')
-exec(open("./code/plot-gini.py", encoding = "utf-8").read())
+exec(open("./code/plot-gini.py", encoding="utf-8").read())
 
 
-# 
-
-# Gaps salariais de raça e gênero (OBS.: só rode se RAM > 32GB) [rodei no servidor, por isso vou deixar comentado]
+# Gaps salariais de raça e gênero (OBS.: só rode se RAM > 32GB) [rodei no servidor,
+# por isso vou deixar comentado]
 # r['source']('code/regs-municipais.R')
 # exec(open("./code/plot-gaps.py", encoding = "utf-8").read())
